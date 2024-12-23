02946a4e5b2dbe1f9c6f2d3fcdcc2867eae0ae62
Uday Shastrakar <uday.shastrakar@gmail.com>
2024-12-23 17:31:37 +0530

    Added spring boot project using kotlin

---
+++ src/main/kotlin/com/app/hosptial/HosptialApplication.kt
@@ -0,0 +1,11 @@
+package com.app.hosptial
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HosptialApplication
+
+fun main(args: Array<String>) {
+	runApplication<HosptialApplication>(*args)
+}
